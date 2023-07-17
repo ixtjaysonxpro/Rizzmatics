1976b64b5903e16a3accb2ab9e13ed60db9403cf
ixtjaysonxpro <123240611+ixtjaysonxpro@users.noreply.github.com>
2023-07-17 09:53:32 +0300

    Create get_location.php

---
+++ get_location.php
@@ -0,0 +1,25 @@
+<?php
+
+// Retrieve the IP address of the visitor
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// Use an IP geolocation service to obtain location information
+$url = "http://ip-api.com/json/{$ip}";
+$response = file_get_contents($url);
+$data = json_decode($response);
+
+// Extract relevant location details
+$country = $data->country ?? '';
+$region = $data->regionName ?? '';
+$city = $data->city ?? '';
+$zip = $data->zip ?? '';
+
+// Create a string with the location information
+$location = "{$city}, {$region}, {$country}, {$zip}";
+
+// Write the location data to a text file
+$file = fopen("location.txt", "w");
+fwrite($file, $location);
+fclose($file);
+
+?>
